96d30eb415aec1ec76baab93c9ee271ef326f90c
ditoma18 <parfaitakounda@.com>
2025-07-23 21:06:10 +0000

    Premier commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,43 @@
+# Exercice 2
+# 1 python version 
+
+# 2 operations
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#3 afichage
+print('parfait')
+print('AKOUNDA')
+print('Togo')
+print('I am enjoying 30 days of python')
+
+# 4 verification of data type
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('parfait'))
+print(type('AKOUNDA'))
+print(type('Togo'))
+
+#Exercice 3
+#Example for different Python data types such as :
+#Integer: 6
+#Float: 7.2
+#Complex: 2+8j
+#String : "python"
+#Boolean : True
+#List: ["Togo", "Benin", "Ghana", "Mali"]
+#Tuple :(Blue, "Yellow", "Green", "Black")
+#Set: {15, 3.3, 0, 2}
+#Dictionary : {"Africa":"Togo", "Europe":"Portugal"}
+
+#Euclidian distance between (2, 3) and (10, 8)
+# code
+print(((10-2)*2-(8-3)*2)**0.5)
